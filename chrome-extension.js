50ad0dd82d6dc6c6ecbcf467ebc513bdbb0ae926
Rabbipxd <130862563+RabbiPND@users.noreply.github.com>
2023-06-11 19:39:18 +0200

    updated extension
    updated extension

--- chrome-extension.js
+++ chrome-extension.js
@@ -5,27 +5,20 @@
  const inputBtn = document.getElementById("input-btn")
  const ulEl = document.getElementById('ul-el')
 
- //myLeads = JSON.parse(myLeads) // (``)
-
- myLeads = JSON.stringify(myLeads)
-
- myLeads.push("uiehfiuarehfurh")
-console.log(myLeads)
-
+ let leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'));
  
- //console.log(typeof myLeads)
 
-
-
-
- //console.log(localStorage.getItem("myLeads"))
- //localStorage.removeItem("myLeads")
 
 inputBtn.addEventListener('click', function() {
   myLeads.push(inputEl.value)
   inputEl.value = ""
+
+  localStorage.setItem('myLeads', JSON.stringify(myLeads)) //save to local storage
+
   renderLeads ()
-  
+
+  console.log(localStorage.getItem("myLeads")) //fetch from local storage
+  console.log(leadsFromLocalStorage)
 });
 
 
@@ -46,7 +39,36 @@ function renderLeads() {
 
 
 
+
+
+
+
+
+
+
+
+
+
+/*                Extra stuff(not applicable to the chrome extension)
+
+
  //               or
 // const li = document.createElement("li")
 // li.textContent = myLeads[i]
 // ulEl.append(li) 
+
+
+
+//  myLeads = JSON.parse(myLeads) // (``)--Turn to array
+//  myLeads.push("uiehfiuarehfurh") 
+//  myLeads = JSON.stringify(myLeads) // turn to string
+//  console.log(typeof myLeads)
+
+//console.log(myLeads)
+
+ 
+
+ //console.log(localStorage.getItem("myLeads"))
+ //localStorage.removeItem("myLeads")
+
+ */
